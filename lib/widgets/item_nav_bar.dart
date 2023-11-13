63b6061e757501560ca21644fe3e446676aec412
Mosaidur <asifbhuiyan135@gmail.com>
2023-11-13 12:06:00 +0600

    3rd update

--- lib/widgets/item_nav_bar.dart
+++ lib/widgets/item_nav_bar.dart
@@ -41,11 +41,12 @@ class ItemNavBar extends StatelessWidget {
               icon: Icon(
                 CupertinoIcons.cart_badge_plus,
                 color: Colors.white,
+                size: 18,
               ),
               label: Text(
                 "Add to cart",
                 style: TextStyle(
-                  fontSize: 16,
+                  fontSize: 13,
                   fontWeight: FontWeight.bold,
                   color: Colors.white
                 ),
@@ -58,6 +59,33 @@ class ItemNavBar extends StatelessWidget {
                 shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                   RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(30)
+                  ),
+                ),
+              ),
+            ),
+            ElevatedButton.icon(
+              onPressed: () {},
+              icon: Icon(
+                CupertinoIcons.bag_fill_badge_plus,
+                color: Colors.white,
+                size: 18,
+              ),
+              label: Text(
+                "Buy Now",
+                style: TextStyle(
+                    fontSize: 13,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white
+                ),
+              ),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.blue),
+                padding: MaterialStateProperty.all(
+                  EdgeInsets.symmetric(vertical: 13, horizontal: 13),
+                ),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30)
                   ),
                 ),
               ),
